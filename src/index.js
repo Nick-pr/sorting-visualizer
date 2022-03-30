46db82e56850a618d0ba847e504076e4b34eb12d
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-03-30 19:34:10 -0400

    Updated to react 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const Root = ReactDOM.createRoot(container);
+Root.render(<App />);
